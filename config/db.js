8481f97fe1202cacdada0c9bcd55f33219223799
sourabhcse <sourabhsaini201098@gmail.com>
2025-02-18 14:49:54 +0530

    initial commit

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to MongoDB");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log("MongoDB connection error: ", err);
+});
+
+module.exports = mongoose;
---
+++ controllers/authController.js
@@ -0,0 +1,39 @@
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const { hashPassword, comparePassword } = require("../utils/hash");
+
+exports.register = async (req, res) => {
+  try {
+    const { name, email, phone, company, password } = req.body;
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) return res.status(400).json({ message: "User already exists" });
+
+    const hashedPassword = await hashPassword(password);
+
+    const newUser = new User({ name, email, phone, company, password: hashedPassword });
+    await newUser.save();
+
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) return res.status(400).json({ message: "User not found" });
+
+    const isMatch = await comparePassword(password, user.password);
+    if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
+
+    res.json({ message: "Login successful", token });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
